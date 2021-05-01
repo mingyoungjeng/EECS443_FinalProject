d051614eac33cc0ba2af436ec9a88084c532f9d8
Mingyoung Jeng <mingyoungjeng@gmail.com>
2021-05-01 11:54:35 -0500

    finished initial lock

--- EECS443_FinalProject.runs/synth_1/.Xil/top_level_propImpl.xdc
+++ EECS443_FinalProject.runs/synth_1/.Xil/top_level_propImpl.xdc
@@ -1,6 +1,6 @@
 set_property SRC_FILE_INFO {cfile:/home/mingyoungjeng/Downloads/EECS443_FinalProject/EECS443_FinalProject.srcs/constrs_1/imports/Nexys4_Board_Material/Nexys4DDR_Master.xdc rfile:../../../EECS443_FinalProject.srcs/constrs_1/imports/Nexys4_Board_Material/Nexys4DDR_Master.xdc id:1} [current_design]
 set_property src_info {type:XDC file:1 line:7 export:INPUT save:INPUT read:READ} [current_design]
-set_property -dict { PACKAGE_PIN E3    IOSTANDARD LVCMOS33 } [get_ports { CLK100MHZ }]; #IO_L12P_T1_MRCC_35 Sch=clk100mhz
+set_property -dict { PACKAGE_PIN E3    IOSTANDARD LVCMOS33 } [get_ports { clk }]; #IO_L12P_T1_MRCC_35 Sch=clk100mhz
 set_property src_info {type:XDC file:1 line:33 export:INPUT save:INPUT read:READ} [current_design]
 set_property -dict { PACKAGE_PIN H17   IOSTANDARD LVCMOS33 } [get_ports { locked }]; #IO_L18P_T2_A24_15 Sch=led[0]
 set_property src_info {type:XDC file:1 line:60 export:INPUT save:INPUT read:READ} [current_design]
--- EECS443_FinalProject.srcs/sources_1/imports/imports/design_sources/top_level.vhd
+++ EECS443_FinalProject.srcs/sources_1/imports/imports/design_sources/top_level.vhd
@@ -8,7 +8,7 @@ ENTITY top_level IS
 	     BTNC, BTNL, BTNR      : IN  STD_LOGIC;
 	     AN        : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
 	     CA        : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
-	     locked: out std_logic
+	     unlocked: out std_logic
 	    );
 END top_level;
 
@@ -32,36 +32,53 @@ ARCHITECTURE behavior OF top_level IS
     CONSTANT n_digits  : NATURAL := 8;    -- 8 7Segment Digits
 	
     signal data, data_next: STD_LOGIC_VECTOR(4*n_digits - 1 DOWNTO 0) := (others =>'0');
-    signal active_seg : natural := n_digits-1;
-    signal direction: std_logic := '0'; -- 0 is "clockwise", 1 is "anticlockwise"
-    signal combination: natural := 16#ABCD#;
+    signal active_seg, active_seg_next: natural := n_digits-1;
+    signal direction: std_logic := '1'; -- 0 is "clockwise", 1 is "anticlockwise"
+    signal combination: natural := 16#1234ABCD#;
+    signal inc, dec: std_logic_vector(3 downto 0) := (others => '0');
+    signal BTNL_state, BTNR_state: std_logic := '0';
 
 BEGIN
-    process (BTNL, BTNR, BTNC)
-    variable active_data: std_logic_vector(3 downto 0) := data(4*active_seg+3 downto 4*active_seg);
+    process (BTNL, BTNR, BTNC, clk)
     begin
-        if (BTNL'event and BTNL='1') then
-            if (direction = '1') then
-                data(4*active_seg+3 downto 4*active_seg) <= std_logic_vector(unsigned(active_data) + 1);
-            else
-                active_seg <= (active_seg + 7) mod n_digits;
+        if (clk'event and clk = '1') then   
+            if (BTNL='1' and BTNL_state='0') then
+                if (direction = '0') then
+                    active_seg <= active_seg_next;
+                    direction <= '1';
+                    data(4*active_seg_next+3 downto 4*active_seg_next) <= inc;
+                else
+                    data(4*active_seg+3 downto 4*active_seg) <= inc;
+                end if;
+            end if;     
+           
+            if (BTNR='1' and BTNR_state='0') then
+                if (direction = '1') then
+                    active_seg <= active_seg_next;
+                    direction <= '0';
+                    data(4*active_seg_next+3 downto 4*active_seg_next) <= dec;
+                else
+                    data(4*active_seg+3 downto 4*active_seg) <= dec;
+                end if;
             end if;
-        end if;     
-        
-        if (BTNR'event and BTNR='1') then
-            if (direction = '0') then
-                data(4*active_seg+3 downto 4*active_seg) <= std_logic_vector(unsigned(active_data) - 1);
-            else
-                active_seg <= (active_seg + 7) mod n_digits;
+            
+            if (BTNC = '1') then
+                if (data = std_logic_vector(to_unsigned(combination, data'length))) then
+                    unlocked <= '1';
+                else
+                    unlocked <= '0';
+                end if;
             end if;
-        end if;
-        
-        locked <= '0';
-        if (BTNC = '1' and data = std_logic_vector(to_unsigned(combination, data'length))) then
-            locked <= '1';
+            
+            BTNL_state <= BTNL;
+            BTNR_state <= BTNR;
         end if;
     end process;
-
+    
+    active_seg_next <= (active_seg + 7) mod n_digits;
+    inc <= std_logic_vector(unsigned(data(4*active_seg+3 downto 4*active_seg)) + 1);
+    dec <= std_logic_vector(unsigned(data(4*active_seg+3 downto 4*active_seg)) - 1);
+    
     i0 : seven_segment_driver
          GENERIC MAP (f_board => f_board,
                       f_flicker => f_flicker,
